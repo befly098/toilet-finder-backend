7598e3f8ebbfe1a19631b9ec72608fb913e2bef9
seorin <befly098@gmail.com>
2025-10-21 14:13:59 +0900

    [chore] spring boot init

---
+++ src/main/java/io/seorin/ddongkan/DdongkanApplication.java
@@ -0,0 +1,13 @@
+package io.seorin.ddongkan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdongkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdongkanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/seorin/ddongkan/DdongkanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seorin.ddongkan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdongkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
