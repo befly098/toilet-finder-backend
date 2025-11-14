38ab6302192cc2daaca077c1e62e8de3342000cb
seorin <befly098@gmail.com>
2025-11-15 08:48:32 +0900

    [chore] 미사용 entity 제거

--- src/main/java/io/seorin/ddongkan/entity/ReportType.java
+++
@@ -1,9 +0,0 @@
-package io.seorin.ddongkan.entity;
-
-public enum ReportType {
-	CROWDED,        // 혼잡도 제보
-	INFO_UPDATE,    // 정보 수정 제보 (영업시간/주소 등)
-	CLOSED,         // 폐점/이용불가
-	DIRTY,          // 위생 문제 제보
-	OTHER           // 기타
-}
