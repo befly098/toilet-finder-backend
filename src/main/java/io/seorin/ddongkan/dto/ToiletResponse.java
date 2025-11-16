293ff8dc0324aa8d31f10cdf246011c9e23a4926
seorin <befly098@gmail.com>
2025-11-16 22:45:41 +0900

    [refactoring] ToiletResponse 불필요한 필드 제거

--- src/main/java/io/seorin/ddongkan/dto/ToiletResponse.java
+++ src/main/java/io/seorin/ddongkan/dto/ToiletResponse.java
@@ -7,29 +7,15 @@ import io.seorin.ddongkan.entity.Toilet;
 public record ToiletResponse(
 	Long id,
 	String name,
-	String address,
 	Double lat,
-	Double lng,
-	String openHours,
-	Boolean accessible,
-	Boolean diaper,
-	Boolean unisex,
-	int avgRating,
-	int avgCleanliness
+	Double lng
 ) {
 	public static ToiletResponse from(Toilet toilet) {
 		return ToiletResponse.builder()
 			.id(toilet.getId())
 			.name(toilet.getName())
-			.address(toilet.getAddress())
 			.lat(toilet.getPosition() != null ? toilet.getPosition().getY() : null)
 			.lng(toilet.getPosition() != null ? toilet.getPosition().getX() : null)
-			.openHours(toilet.getOpenHours())
-			.accessible(toilet.getAccessible())
-			.diaper(toilet.getDiaper())
-			.unisex(toilet.getUnisex())
-			.avgRating(toilet.getAvgRating())
-			.avgCleanliness(toilet.getAvgCleanliness())
 			.build();
 	}
 
@@ -40,15 +26,8 @@ public record ToiletResponse(
 	public static class Builder {
 		private Long id;
 		private String name;
-		private String address;
 		private Double lat;
 		private Double lng;
-		private String openHours;
-		private Boolean accessible;
-		private Boolean diaper;
-		private Boolean unisex;
-		private int avgRating;
-		private int avgCleanliness;
 
 		public Builder id(Long id) {
 			this.id = id;
@@ -57,11 +36,6 @@ public record ToiletResponse(
 
 		public Builder name(String name) {
 			this.name = name;
-			return this;
-		}
-
-		public Builder address(String address) {
-			this.address = address;
 			return this;
 		}
 
@@ -75,49 +49,12 @@ public record ToiletResponse(
 			return this;
 		}
 
-		public Builder openHours(String openHours) {
-			this.openHours = openHours;
-			return this;
-		}
-
-		public Builder accessible(Boolean accessible) {
-			this.accessible = accessible;
-			return this;
-		}
-
-		public Builder diaper(Boolean diaper) {
-			this.diaper = diaper;
-			return this;
-		}
-
-		public Builder unisex(Boolean unisex) {
-			this.unisex = unisex;
-			return this;
-		}
-
-		public Builder avgRating(int avgRating) {
-			this.avgRating = avgRating;
-			return this;
-		}
-
-		public Builder avgCleanliness(int avgCleanliness) {
-			this.avgCleanliness = avgCleanliness;
-			return this;
-		}
-
 		public ToiletResponse build() {
 			return new ToiletResponse(
 				id,
 				name,
-				address,
 				lat,
-				lng,
-				openHours,
-				accessible,
-				diaper,
-				unisex,
-				avgRating,
-				avgCleanliness
+				lng
 			);
 		}
 	}
