888a64e2f270a12a6f79dccfe0ca0e36a97e23a6
seorin <befly098@gmail.com>
2026-04-05 16:05:02 +0900

    [chore] swagger base url 변경 localhost:8080/docs로 접근하면 docs 페이지가 나오도록 변경

--- src/main/java/io/seorin/ddongkan/config/StaticRoutingConfig.java
+++ src/main/java/io/seorin/ddongkan/config/StaticRoutingConfig.java
@@ -1,18 +1,21 @@
 package io.seorin.ddongkan.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticRoutingConfig implements WebMvcConfigurer {
-	private static final Logger log = LoggerFactory.getLogger(StaticRoutingConfig.class);
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/docs").setViewName("forward:/swagger-ui.html");
+		registry.addViewController("/docs/").setViewName("forward:/swagger-ui.html");
 	}
 }
