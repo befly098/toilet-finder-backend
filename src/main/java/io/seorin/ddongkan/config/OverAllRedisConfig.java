f5d8bf67c7dd11e30c27b467f2b920a5199fe25f
seorin <befly098@gmail.com>
2026-05-17 22:20:26 +0900

    [fix] RedisConfig 파일을 수정합니다.. Lettuce Factory를 사용하도록 수정했습니다.

---
+++ src/main/java/io/seorin/ddongkan/config/OverAllRedisConfig.java
@@ -0,0 +1,67 @@
+package io.seorin.ddongkan.config;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class OverAllRedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	@Bean
+	public LettuceConnectionFactory lettuceConnectionFactory() {
+
+		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+			.commandTimeout(Duration.ofSeconds(2))
+			.shutdownTimeout(Duration.ZERO)
+			.build();
+
+		return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port), clientConfig);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper()
+			.registerModule(new JavaTimeModule())
+			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+			.entryTtl(Duration.ofMinutes(10)) // 캐시 항목의 TTL 설정
+			.computePrefixWith(cacheName -> cacheName + ":")
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()) // 키는 문자열로 직렬화
+			)
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()))
+			)
+			.disableCachingNullValues(); // null 값 캐싱 비활성화
+
+		return RedisCacheManager.builder(connectionFactory)
+			.cacheDefaults(defaultConfig)
+			.build();
+	}
+}
